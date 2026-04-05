a40fc64fe53bd703815028358418db1b5493ea66
Siyoung Park <siyoung.park18@gmail.com>
2026-04-05 14:32:02 -0600

    Minor Change

--- Log.py
+++ Log.py
@@ -156,7 +156,7 @@ class LogRedirect(io.TextIOBase):
     def write(self, msg):
         msg = msg.rstrip('\n')
         if not msg.strip():
-            if self.app.config.get("log_blank_lines", True):
+            if self.app.config.get("log_blank_lines", False):
                 self.log.configure(state='normal')
                 hide_tag = self._active_section["hide_tag"] if self._active_section else None
                 if hide_tag:
