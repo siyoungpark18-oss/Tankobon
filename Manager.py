fccc9cdc66b357a54599fe93a9f5a7713085dafa
Siyoung Park <siyoung.park18@gmail.com>
2026-04-06 11:46:59 -0600

    Update Manager.py

--- Manager.py
+++ Manager.py
@@ -928,7 +928,7 @@ def find_duplicates(config, cancel=None):
             print("")
             return
         try:
-            digest = hashlib.lib.sha256(f.read_bytes()).hexdigest()
+            digest = hashlib.sha256(f.read_bytes()).hexdigest()
             file_digests[f] = digest
             if digest in hashes:
                 duplicates.add(f)
